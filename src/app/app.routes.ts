674f0595957c64d626aa69a4120e30486cf9ec33
saaim <saaimasrar0@gmail.com>
2025-05-06 22:47:09 +0500

    made views and refined code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import { ShopComponent } from './shop/shop.component';
+import { HomeComponent } from './views/home/home.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { ShopComponent } from './views/shop/shop.component';
 
 export const routes: Routes = [
     {path: "home",component:HomeComponent},
