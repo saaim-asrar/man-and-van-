b0242f0b321a6e9d08b18c30670f3809dab5dcfe
saaim <saaimasrar0@gmail.com>
2025-05-21 21:54:23 +0500

    trying to connect database with form component

--- src/app/views/contact/contact.component.ts
+++ src/app/views/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../api.service';
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+users:any[]=[]
+constructor (private apiService:ApiService){}
+  
+ngOnInit(): void {
+  this.getUsers()
+  }
 
+
+getUsers():void{
+  this.apiService.getStudents().subscribe({
+    next:(res)=>this.users=res,
+    error:(err)=>console.error('error loading users:',err)
+  })
+}
 }
